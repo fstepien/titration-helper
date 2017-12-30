fbce248ece7387472364ac80d0b4d674d19c27e1
Filip Stepien <contact@filipstepien.com>
2017-12-30 10:25:44 -0500

    updated Titration Calculation with event deligation so that it works on JS generated procedure cards.

--- js/app.js
+++ js/app.js
@@ -70,14 +70,100 @@ function addNewCard(e){
 const newCard = document.createElement('div');
 newCard.className = 'card larger';
 newCard.style = 'margin-top: 3em';
-newCard.innerHTML = `<div>Insert Secondary Card Here</div>`;     
+newCard.innerHTML = `<div class="card-content">
+<div class="row">
+    <div class="col s10">  
+    <span class="card-title">Electroless Nickel - Nickel Metal Analysis</span>
+    </div>
+    <div class="col s2 right-align">
+      <a class="btn-floating waves-effect waves-light red remove-card"><i class="material-icons">close</i></a>
+    </div>  
+</div>    
+
+  <ol class="collection" id="procedure-list">
+    <li class="collection-item">Pippet 5ml operating solution into 250ml Erlenmeyer 
+      <span class="secondary-content">
+        <form><p>
+        <input type="checkbox" id="checkbox1"/>
+        <label for="checkbox1"></label>
+        </form></span>
+    </li>
+    <li class="collection-item">Add 10mL Ammonium Hydroxide (AG Grade)
+        <span class="secondary-content">
+            <form><p>
+            <input type="checkbox" id="checkbox2"/>
+            <label for="checkbox2"></label>
+            </form></span>
+    </li>
+    <li class="collection-item">Add a pinch Murexide to pale straw color
+        <span class="secondary-content">
+            <form><p>
+            <input type="checkbox" id="checkbox3"/>
+            <label for="checkbox3"></label>
+            </form></span>
+    </li>
+    <li class="collection-item">Titrate with 0575M EDTA to blue-purple endpoint
+        <span class="secondary-content">
+            <form><p>
+            <input type="checkbox" id="checkbox4"/>
+            <label for="checkbox4"></label>
+            </form></span>
+    </li>
+    <li class="collection-item">Record mL 0.0575M EDTA titrated as "A"
+        <span class="secondary-content">
+            <form><p>
+            <input type="checkbox" id="checkbox5"/>
+            <label for="checkbox5"></label>
+            </form></span>
+    </li>
+  </ol> 
+
+  <div class="card-content titration-color" style="background: linear-gradient(to right, orange, #2A00BB)">
+  </div>
+
+  <div class="card-content">
+  <form class="calc-form">
+    <!--INPUT 1 ENTER A-->
+    <div class="row">
+        <div class="input-field col s12 m 6 l6 input-ab">
+            <i class="prefix">A</i>
+            <input class="input-a" type="number">
+            <label for="icon_prefix">mL 0.0575M EDTA titrated</label>
+          </div>
+       <!--INPUT 2 ENTER B-->
+        <div class="input-field col s12 m6 l6 input-ab">
+              <i class="prefix">B</i>
+              <input class="input-b" type="number" disabled>
+              <label for="icon_prefix"></label>
+        </div>
+     </div>
+      <!--INPUT 2 ANSWER IMPERIAL-->
+            <div class="row answer-imperial-row">
+                <div class="col s12"> A x 0.09 = <div class="input-field inline"><input type="number" class="answer-imperial center-align" style="width: 4em" disabled></div>oz/gal of Nickel Metal.  Min: <div class="input-field inline"><input type="number" placeholder="0.7" class="min-imperial center-align" style="width: 4em"disabled></div> Max: <div class="input-field inline">
+                    <input type="number" placeholder="0.8" class="max-imperial center-align" style="width: 4em" disabled></div></div>
+              
+            </div>
+      <!--INPUT 2 ANSWER METRIC-->
+            <div class="row" class="answer-metric-row">
+                <div class="col s12">A x 0.675 = 
+                  <div class="input-field inline">
+                    
+                    <input type="number" class="center-align validate answer-metric" style="width: 4em" disabled>
+                  </div>g/L of Nickel Metal.
+                Min: <div class="input-field inline"><input class="min-metric center-align" type="number" placeholder="5.25"  style="width: 4em"disabled></div> Max: <div class="input-field inline">
+                  <input class="max-metric center-align" type="number" placeholder="6" style="width: 4em" disabled></div></div></div>
+            </div>
+        
+  </form>
+  </div>  
+  </div>`;     
 const mainSection = document.querySelector('.main-section');
 mainSection.appendChild(newCard);       
 }
 
 //Remove card from HTML 
 
-document.querySelector('.card').addEventListener('click', removeCard);
+document.body.addEventListener('click', removeCard);
 
 function removeCard(e){
 if(e.target.parentElement.classList.contains('remove-card')){
@@ -91,41 +177,50 @@ if(e.target.parentElement.classList.contains('remove-card')){
 //Titration Application Calculation
 
 
-document.querySelector('.input-a').addEventListener('keyup', calculateResults);
+document.body.addEventListener('keyup', calculateResults);
 
 function calculateResults(e){
+
+  if(e.target.classList.contains('input-a')){
+// previously listened to querySelector('.input-a')
  
-  const valueA = parseFloat(document.querySelector('.input-a').value);
+  const valueA = parseFloat(e.target.value);
   //const valueB = document.getElementById('input-b');
-  const answerImperial = document.querySelector('.answer-imperial');
-  const answerMetric = document.querySelector('.answer-metric');
+
+  //Previously const answerImperial = document.querySelector('.answer-imperial'); which does not work on JS entered forms
+  const answerImperial = e.target.parentElement.parentElement.parentElement.children[1].children[0].children[0].children[0];
+  //Previously const answerMetric = document.querySelector('.answer-metric'); which doesn't work on a JS imputed form
+  const answerMetric = e.target.parentElement.parentElement.parentElement.children[2].children[0].children[0].children[0];
+
+  //used to find the correct target.... will be updated once i find a better way...console.log(e.target.parentElement.parentElement.parentElement.children[2].children[0].children[0].children[0]);
 
   if(Number.isNaN(valueA)){
  console.log('NaN');
   } else {
-                console.log(valueA);
+                //console.log(valueA);
               answerImperial.value = (valueA * 0.09).toFixed(2);
               if(answerImperial.value < 0.7 || answerImperial.value > 0.8) {
                 answerImperial.style.color = "red";
-                console.log(`${answerImperial.value}g/L is OUT of range`);
+                //console.log(`${answerImperial.value}oz/gal is OUT of range`);
               } else {
                 answerImperial.style.color = "green";
-                console.log(`${answerImperial.value}g/L is IN of range`);
+                //console.log(`${answerImperial.value}oz/gal is IN of range`);
               }
 
               answerMetric.value = (valueA * 0.675).toFixed(2);
               if(answerMetric.value < 5.25 || answerMetric.value > 6) {
                 answerMetric.style.color = "red";
-                console.log(`${answerMetric.value}g/L is OUT of range`);
+                //console.log(`${answerMetric.value}g/L is OUT of range`);
               } else {
                 answerMetric.style.color = "green";
-                console.log(`${answerMetric.value}g/L is IN of range`);
+                //console.log(`${answerMetric.value}g/L is IN of range`);
               } 
   } 
 }
-document.querySelector('body').addEventListener('click', runTest)
-function runTest(e){
-console.log('start test');
-
-console.log('end test');
 }
+// document.querySelector('body').addEventListener('click', runTest)
+// function runTest(e){
+// console.log('start test');
+
+// console.log('end test');
+// }
