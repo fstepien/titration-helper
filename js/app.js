aa37b0fedff1ceddd381d8b9f0c3cd6e11763dab
Filip Stepien <contact@filipstepien.com>
2018-01-04 07:48:50 -0500

    re-arrange producedures to give global scope, starting to work on calculations pulled from procedures

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ class Procedure {
 
           const newCard = document.createElement('div');
           newCard.className = 'card larger main-card';
+          newCard.id = `${this.id}-card`;
           newCard.style = 'margin-top: 3em';
           newCard.innerHTML = `<div class="card-content">
           <div class="row">
@@ -116,62 +117,70 @@ class Procedure {
           step.appendChild(checkbox);
           ol.appendChild(step);     
           })}
+
+  performCalc() {
+    console.log('add calc here');
+  }        
  
 }
+
+//Global Variable - to be pulled from DB in the future
+const enHypophosphite = new Procedure(
+  /*ID*/'enHypophosphite',
+  /*title*/'Electroless Nickel - Hypophosphite Analysis', 
+  /*procedure list*/ 
+  ['Pipet a 5 ml sample of the solution into a 250ml glass stoppered iodine flask', 
+  'Add 25 mL of 6N HCL, using it to rinse down the neck of the flask.', 
+  'Pipet 50 mL of the 0.10N KIO3 solution into the flask. Add one gram of KI crystals.',
+  'Rinse down the neck of the flask with 3 to 5 mL of 6N HCl',
+  'Stopper the flask and set in the dark for EXACTLY 30 min',
+  'Rinse stopper and neck of flask with a small amount of deionized water',
+  'Titrate immediately with 0.1N Sodium Thiosulfate Soln. from Brown to Colorless'
+  ],
+  /*color1*/ '#604020', 
+  /*color 2*/'white',
+  /*inputA*/"mL Sodium Thisulfate Solution", 
+  /*inputB*/'Not Required', 
+  /*inputBState*/ null, 
+  /*resultsImperial*/ 'calc 1 currently string', 
+  /* min Imperial*/ 2.5,
+  /* max Imperial*/ 3.3,
+  /*resultsMetric*/ 'calc 2 currently stringt',
+  /* min Metric*/ 19,
+  /* max Metric*/ 25);  
+
+  const enNickel = new Procedure(
+    /*ID*/'enNickel',
+    /*title*/'Electroless Nickel - Metal Analysis', 
+    /*procedure list*/ 
+    ['Pippet 5ml operating solution into 250ml Erlenmeyer',
+    'Add 10mL Ammonium Hydroxide (AG Grade)',
+    'Add a pinch Murexide to pale straw color',
+    'Titrate with 0575M EDTA to blue-purple endpoint',
+    'Record mL 0.0575M EDTA titrated as "A"'
+    ],
+    /*color1*/ 'orange', 
+    /*color 2*/'#2A00BB',
+    /*inputA*/"mL 0.0575M EDTA titrated", 
+    /*inputB*/'Not Required', 
+    /*inputBState*/ null, 
+    /*resultsImperial*/ 'calc 1 currently string', 
+    /* min Imperial*/ 0.7,
+    /* max Imperial*/ 0.8,
+    /*resultsMetric*/ 'calc 2 currently stringt',
+    /* min Metric*/ 5.25,
+    /* max Metric*/ 6);  
+
 // Insert card to HTML
 
 document.body.addEventListener('click', addNewCard);
 
 function addNewCard(e){
   if(e.target.parentElement.classList.contains('enHypophosphite')){
-  const enHypophosphite = new Procedure(
-    /*ID*/'enHypophosphite',
-    /*title*/'Electroless Nickel - Hypophosphite Analysis', 
-    /*procedure list*/ 
-    ['Pipet a 5 ml sample of the solution into a 250ml glass stoppered iodine flask', 
-    'Add 25 mL of 6N HCL, using it to rinse down the neck of the flask.', 
-    'Pipet 50 mL of the 0.10N KIO3 solution into the flask. Add one gram of KI crystals.',
-    'Rinse down the neck of the flask with 3 to 5 mL of 6N HCl',
-    'Stopper the flask and set in the dark for EXACTLY 30 min',
-    'Rinse stopper and neck of flask with a small amount of deionized water',
-    'Titrate immediately with 0.1N Sodium Thiosulfate Soln. from Brown to Colorless'
-    ],
-    /*color1*/ '#604020', 
-    /*color 2*/'white',
-    /*inputA*/"mL Sodium Thisulfate Solution", 
-    /*inputB*/'Not Required', 
-    /*inputBState*/ null, 
-    /*resultsImperial*/ 'calc 1 currently string', 
-    /* min Imperial*/ 2.5,
-    /* max Imperial*/ 3.3,
-    /*resultsMetric*/ 'calc 2 currently stringt',
-    /* min Metric*/ 19,
-    /* max Metric*/ 25);  
     enHypophosphite.newProcedureCard();
     enHypophosphite.addProcedureList();
   }
   if(e.target.parentElement.classList.contains('enNickel')){
-    const enNickel = new Procedure(
-      /*ID*/'enNickel',
-      /*title*/'Electroless Nickel - Metal Analysis', 
-      /*procedure list*/ 
-      ['Pippet 5ml operating solution into 250ml Erlenmeyer',
-      'Add 10mL Ammonium Hydroxide (AG Grade)',
-      'Add a pinch Murexide to pale straw color',
-      'Titrate with 0575M EDTA to blue-purple endpoint',
-      'Record mL 0.0575M EDTA titrated as "A"'
-      ],
-      /*color1*/ 'orange', 
-      /*color 2*/'#2A00BB',
-      /*inputA*/"mL 0.0575M EDTA titrated", 
-      /*inputB*/'Not Required', 
-      /*inputBState*/ null, 
-      /*resultsImperial*/ 'calc 1 currently string', 
-      /* min Imperial*/ 0.7,
-      /* max Imperial*/ 0.8,
-      /*resultsMetric*/ 'calc 2 currently stringt',
-      /* min Metric*/ 5.25,
-      /* max Metric*/ 6);  
       enNickel.newProcedureCard();
       enNickel.addProcedureList();
     }
@@ -192,7 +201,6 @@ if(e.target.parentElement.classList.contains('remove-card')){
 
 //Titration Application Calculation
 
-
 document.body.addEventListener('keyup', calculateResults);
 
 function calculateResults(e){
@@ -207,28 +215,31 @@ function calculateResults(e){
       const minMetric      = e.target.parentElement.parentElement.parentElement.children[2].children[0].children[1].children[0].placeholder;
       const maxMetric      = e.target.parentElement.parentElement.parentElement.children[2].children[0].children[2].children[0].placeholder;
       
-      if(Number.isNaN(valueA)){
-      console.log('NaN'); 
-      } else {    //Calculation Imperial ****** DANGER FIX ME *******
-                  answerImperial.value = (valueA * 0.09).toFixed(2);
-                  //Validation Metric
-                  if(answerImperial.value < minImpertial || answerImperial.value > maxImperial) {
-                    answerImperial.style.color = "red";
-                  
-                  } else {
+          if(Number.isNaN(valueA)){
+          console.log('NaN'); 
+          } else {    //Calculation Imperial ****** DANGER FIX ME *******
+                      answerImperial.value = (valueA * 0.09).toFixed(2);
+                      // DELETE ME - enHypophosphite displays so pull 
+                      console.log(e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+                      enHypophosphite.performCalc()
+                      //Validation Metric
+                      if(answerImperial.value < minImpertial || answerImperial.value > maxImperial) {
+                        answerImperial.style.color = "red";
+                      
+                      } else {
 
-                    answerImperial.style.color = "green";
-                    
-                  }
-                  //Calciulation Metric ****** DANGER FIX ME *******
-                  answerMetric.value = (valueA * 0.675).toFixed(2);
-                  //Validation Metric
-                  if(answerMetric.value < minMetric || answerMetric.value > maxMetric) {
+                        answerImperial.style.color = "green";
+                        
+                      }
+                      //Calciulation Metric ****** DANGER FIX ME *******
+                      answerMetric.value = (valueA * 0.675).toFixed(2);
+                      //Validation Metric
+                      if(answerMetric.value < minMetric || answerMetric.value > maxMetric) {
 
-                    answerMetric.style.color = "red";
-                  } else {
-                    answerMetric.style.color = "green";
-                  } 
-      } 
+                        answerMetric.style.color = "red";
+                      } else {
+                        answerMetric.style.color = "green";
+                      } 
+          } 
     }
 }
